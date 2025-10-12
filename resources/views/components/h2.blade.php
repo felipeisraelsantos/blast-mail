0f32b8fa5813f3f10abe990ea77a4a118c4096cd
felipeisraelsantos <felipe.israel@aluno.ifsp.edu.br>
2025-10-11 21:37:13 -0300

    fix: startand projeto por erro de enconding

--- resources/views/components/h2.blade.php
+++
@@ -1,3 +0,0 @@
- <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-     {{ $slot }}
- </h2>
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
-
-Route::view('/dashboard','dashboard')->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::get('/', function () {
+    return view('welcome');
 });
-
-require __DIR__.'/auth.php';

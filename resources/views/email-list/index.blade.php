c692facb63a787fd6257dc5ef11523f601d46d02
felipeisraelsantos <felipe.israel@aluno.ifsp.edu.br>
2025-10-13 23:58:45 -0300

    feat: criado lista de emails

--- resources/views/email-list/index.blade.php
+++ resources/views/email-list/index.blade.php
@@ -10,6 +10,7 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     @forelse ($emailLists as $list)
+                     teste
                     @empty
                         <div class="flex justify-center">
                             <x-link-button :href="route('email-list.create')">

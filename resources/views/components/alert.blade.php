b97774dbe7ccb2a92c9f99b94022b4063a49f3e6
felipeisraelsantos <felipe.israel@aluno.ifsp.edu.br>
2026-04-22 08:22:11 -0300

    fix: corrigindo componente

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -4,7 +4,7 @@
     'info' => null,
     'warning' => null,
     'danger' => null,
-    'no-icon' => false,
+    'noIcon' => false,
 ])
 
 <div @class([
@@ -36,12 +36,6 @@
                         clip-rule="evenodd" />
                 </svg>
             @endunless
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-6"
-                aria-hidden="true">
-                <path fill-rule="evenodd"
-                    d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm3.857-9.809a.75.75 0 0 0-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 1 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4-5.5Z"
-                    clip-rule="evenodd" />
-            </svg>
         </div>
         <div class="ml-2">
             <h3 @class([
